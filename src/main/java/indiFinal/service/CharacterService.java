46e972b905a370bc380d684c4fa187b6fa2ceaf4
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-03-30 19:56:06 -0400

    adds fetch single character request call in characterService class within service package

--- src/main/java/indiFinal/service/CharacterService.java
+++ src/main/java/indiFinal/service/CharacterService.java
@@ -1,4 +1,11 @@
 package indiFinal.service;
 
+import indiFinal.model.Character;
+import org.springframework.web.client.RestTemplate;
+
 public class CharacterService {
+    public Character fetchSingleCharacter(int characterID){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("https://swapi.co/api/people/" + characterID, Character.class);
+    }
 }
