b0e41be79d010e05b1e42c44e71d9042a5ffd3f9
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-03-30 19:59:09 -0400

    adds json property annotations to associated setters and getters within character class

--- src/main/java/indiFinal/model/Character.java
+++ src/main/java/indiFinal/model/Character.java
@@ -1,5 +1,9 @@
 package indiFinal.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Character {
     private String name;
     private String birthYear;
@@ -13,26 +17,32 @@ public class Character {
         this.homeworld = homeworld;
     }
 
+    @JsonProperty("name")
     public String getName() {
         return name;
     }
 
+    @JsonProperty("name")
     public void setName(String name) {
         this.name = name;
     }
 
+    @JsonProperty("birth_year")
     public String getBirthYear() {
         return birthYear;
     }
 
+    @JsonProperty("birth_year")
     public void setBirthYear(String birthYear) {
         this.birthYear = birthYear;
     }
 
+    @JsonProperty("gender")
     public String getGender() {
         return gender;
     }
 
+    @JsonProperty("gender")
     public void setGender(String gender) {
         this.gender = gender;
     }
