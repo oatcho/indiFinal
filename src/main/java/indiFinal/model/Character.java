b402935b895a8abe542fcc04b03490c2b03cf7c6
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-03-30 19:01:50 -0400

    adds properties, setters, getters, and constructor for character class

--- src/main/java/indiFinal/model/Character.java
+++ src/main/java/indiFinal/model/Character.java
@@ -1,4 +1,47 @@
 package indiFinal.model;
 
 public class Character {
+    private String name;
+    private String birthYear;
+    private String gender;
+    private String homeworld;
+
+    public Character(String name, String birthYear, String gender, String homeworld) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.gender = gender;
+        this.homeworld = homeworld;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHomeworld() {
+        return homeworld;
+    }
+
+    public void setHomeworld(String homeworld) {
+        this.homeworld = homeworld;
+    }
 }
