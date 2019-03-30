11f9dcb6e2e02866ec5d0eb79bf33762e49bdb0d
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-03-30 19:04:32 -0400

    adds data package to indiFinal app, characterRepository class to data package, and empty arraylist to characterRepository class

---
+++ src/main/java/indiFinal/data/CharacterRepository.java
@@ -0,0 +1,10 @@
+package indiFinal.data;
+
+import indiFinal.model.Character;
+
+import java.util.ArrayList;
+
+public class CharacterRepository {
+    ArrayList<Character> characterArrayList = new ArrayList<>();
+
+}
