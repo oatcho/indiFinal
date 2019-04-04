f70bc4750e9810281fb3fc98d0ee905cef3bf483
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-04-04 14:58:37 -0400

    adds test that tests findByName method in characterRepo class

--- src/test/java/indiFinal/data/CharacterRepositoryTest.java
+++ src/test/java/indiFinal/data/CharacterRepositoryTest.java
@@ -1,5 +1,10 @@
 package indiFinal.data;
 
+import indiFinal.controller.AppController;
+import indiFinal.model.Character;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,6 +12,10 @@ import static org.junit.Assert.*;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
+import static sun.plugin2.main.client.ServiceDelegate.get;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class CharacterRepositoryTest {
 
@@ -25,5 +34,15 @@ public class CharacterRepositoryTest {
     }
 
     @Test
-    public void should
+    public void shouldRetrievesSearchedForCharacter(){
+        charRepo.addAllCharactersToArrayList();
+        Character returnedCharacter = charRepo.findByName("yoda");
+        assertThat(returnedCharacter, equalTo(charRepo.characters.get(9)));
+    }
+
+    @Test
+    public void testsForSuccessfulRequestAndContentType(){
+        given().when().get("https://swapi.co/api/people/1/?format=json").then().assertThat()
+                .statusCode(200).and().contentType(ContentType.JSON);
+    }
 }
